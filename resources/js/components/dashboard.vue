63a7f2e17e56b2d54d8cbfa72c071742998366ee
usaginotsuki <jcchachalo@gmail.com>
2021-07-04 00:01:39 -0500

    New design

--- resources/js/components/dashboard.vue
+++ resources/js/components/dashboard.vue
@@ -1,202 +1,101 @@
 <template>
     <div class="container-fluid">
         <Header-1></Header-1>
-        <div class="row">
-            <div class="col-md-3">
-                <div class="row">
-                    <div class="offset-1"></div>
-                    <div class="col-md">
-                        <img
-                            src="../../assets/Moodle-chapeau_Grande.png"
-                            alt="moodle"
-                            class="im"
-                        />
-                    </div>
-                    <br />
-                </div>
-                <div class="row">
-                    <br />
-                    <div class="offset-3"></div>
-                    <div class="col-md-6">
-                        <br />
-                        <a :href="moodle">Moodle</a>
-                    </div>
-                </div>
-                <hr />
-                <div class="row">
-                    <div class="offset-3"></div>
-                    <div class="col-md-6">
-                        <button
-                            class="btn btn-link"
-                            data-toggle="modal"
-                            data-target="#staticBackdrop"
-                        >
-                            Alumno
-                        </button>
-                    </div>
-                </div>
-                <hr />
-                <div class="row">
-                    <div class="offset-3"></div>
-                    <div class="col-md-6">
-                        <button
-                            class="btn btn-link"
-                            data-toggle="modal"
-                            data-target="#staticBackdrop"
-                        >
-                            Docente
-                        </button>
-                    </div>
-                </div>
-                <hr />
-                <div class="row">
-                    <div class="offset-3"></div>
-                    <div class="col-md-6">
-                        <button
-                            class="btn btn-link"
-                            data-toggle="modal"
-                            data-target="#staticBackdrop"
-                        >
-                            Utics
-                        </button>
-                    </div>
-                </div>
-                <hr />
-                <div class="row">
-                    <div class="offset-3"></div>
-                    <div class="col-md-6">
-                        <button
-                            class="btn btn-link"
-                            data-toggle="modal"
-                            data-target="#staticBackdrop"
-                        >
-                            Financiero
-                        </button>
-                    </div>
-                </div>
-                <hr />
-                <div class="row">
-                    <div class="offset-3"></div>
-                    <div class="col-md-6">
-                        <button
-                            class="btn btn-link"
-                            data-toggle="modal"
-                            data-target="#staticBackdrop"
-                        >
-                            Administrativo
-                        </button>
-                    </div>
-                </div>
-                <hr />
-                <div class="row">
-                    <div class="offset-3"></div>
-                    <div class="col-md-6">
-                        <button
-                            class="btn btn-link"
-                            data-toggle="modal"
-                            data-target="#staticBackdrop"
-                        >
-                            Laboratorios
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-6">
-                        <div class="row">
-                            <div class="col">
-                                <img
-                                    src="../../assets/img1.png"
-                                    class="img-fluid"
-                                    alt="img1"
-                                />
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col">
-                                <img
-                                    src="../../assets/img2.png"
-                                    class="img-fluid"
-                                    width="94.5%"
-                                    alt="img2"
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-6 tl">
-                        <img
-                            src="../../assets/img3.png"
-                            class="img-fluid"
-                            width="86%"
-                            alt="img3"
-                        />
-                    </div>
-                </div>
-            </div>
+        <div class="container">
             
-        </div>
-        <div
-            id="staticBackdrop"
-            class="modal fade"
-            data-backdrop="static"
-            data-keyboard="false"
-            tabindex="-1"
-            aria-labelledby="staticBackdropLabel"
-            aria-hidden="true"
-        >
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="staticBackdropLabel">
-                            {{
-                                role === "alu"
-                                    ? "Alumno"
-                                    : role === "doc"
-                                    ? "Docente"
-                                    : role === "tics"
-                                    ? "UTics"
-                                    : role === "fin"
-                                    ? "Financiero"
-                                    : role === "admin"
-                                    ? "Administrativo"
-                                    : "Laboratorios"
-                            }}
-                        </h5>
-                        <button
-                            type="button"
-                            class="close color-black"
-                            data-dismiss="modal"
-                            aria-label="Close"
-                        >
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body" v-if="role == 'alu'"></div>
-                    <div class="modal-body" v-if="role == 'doc'"></div>
-                    <div class="modal-body" v-if="role == 'tics'"></div>
-                    <div class="modal-body" v-if="role == 'fin'"></div>
-                    <div class="modal-body" v-if="role == 'admin'"></div>
-                    <div class="modal-body" v-if="role == 'lab'"></div>
-                    <div class="modal-body">
-                        <ul class="list-group list-group-flush">
-                            <li
-                                class="list-group-item"
-                                v-for="itm in items[role]"
-                            >
-                                <a :href="itm.url">{{ itm.name }}</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="modal-footer">
-                        <button
-                            type="button"
-                            class="btn btn-secondary"
-                            data-dismiss="modal"
-                        >
-                            Close
-                        </button>
-                    </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <a :href="this.items.sgrad">
+                        <div class="card">
+                            <div class="card-header">
+                                <img
+                                    class="card-img-top"
+                                    src="../../assets/sg-ecp.png"
+                                />
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <hr />
+
+                <div class="col-md-4">
+                    <a :href="this.items.otrs">
+                        <div class="card">
+                            <div class="card-header">
+                                <img
+                                    class="card-img-top"
+                                    src="../../assets/sg-otrs.png"
+                                />
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <hr />
+                <div class="col-md-4">
+                    <a :href="this.items.ecp">
+                        <div class="card">
+                            <div class="card-header">
+                                <img
+                                    class="card-img-top"
+                                    src="../../assets/sg-ecp.png"
+                                />
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <hr />
+            <div class="row">
+                <div class="col-md-4">
+                    <a :href="this.items.telefonia">
+                        <div class="card">
+                            <div class="card-header">
+                                <img
+                                    class="card-img-top"
+                                    src="../../assets/sg-tel.png"
+                                />
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <a :href="this.items.fin">
+                        <div class="card">
+                            <div class="card-header">
+                                <img
+                                    class="card-img-top"
+                                    src="../../assets/sg-fin.png"
+                                />
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <a :href="this.items.educacioncontinua">
+                        <div class="card">
+                            <div class="card-header">
+                                <img
+                                    class="card-img-top"
+                                    src="../../assets/sg-uec.png"
+                                />
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <hr />
+            <div class="row" style="justify-content: center">
+                <div class="col-md-4">
+                    <a :href="this.items.reporteshistoricos">
+                        <div class="card">
+                            <div class="card-header">
+                                <img
+                                    class="card-img-top"
+                                    src="../../assets/sg-ri.png"
+                                />
+                            </div>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
@@ -212,7 +111,7 @@ import "bootstrap";
 export default {
     token: localStorage.getItem("laravel-jwt-auth"),
     //console.log(this.token),
-    //tok:(token.toString()), 
+    //tok:(token.toString()),
     al: String("qwerty"),
     moodle: "http://moodleltga.espe.edu.ec/moodle/login/index.php/",
     data() {
@@ -231,52 +130,30 @@ export default {
           lab: [{name:'SG-RAD',url:"http://webltga.espe.edu.ec/sg-lab/"+((localStorage.getItem('laravel-jwt-auth')))}]
         }*/
             items: {
-                alu: [
-                    {
-                        name: "Moodle Ingenierias",
-                        url: "" + localStorage.getItem("laravel-jwt-auth")
-                    }
-                ],
-                doc: [
-                    {
-                        name: "SG-RAD",
-                        url:
-                            "http://webltga.espe.edu.ec/sg-rad/index.php?r=site/login&token=" +
-                            localStorage.getItem("laravel-jwt-auth")
-                    }
-                ],
-                tics: [
-                    {
-                        name: "OTRS",
-                        url:
-                            "http://webltga.espe.edu.ec/sg-otrs/index.php?r=site/login&token=" +
-                            localStorage.getItem("laravel-jwt-auth")
-                    }
-                ],
-                fin: [
-                    {
-                        name: "Sistema Financiero",
-                        url:
-                            "http://webltga.espe.edu.ec/comprobantes/index.php?r=site/login&token=" +
-                            localStorage.getItem("laravel-jwt-auth")
-                    }
-                ],
-                admin: [
-                    {
-                        name: "Biblioteca",
-                        url:
-                            "http://webltga.espe.edu.ec/sg-ecp/index.php?r=site/login&token=" +
-                            localStorage.getItem("laravel-jwt-auth")
-                    }
-                ],
-                lab: [
-                    {
-                        name: "SG-Telefonia",
-                        url:
-                            "http://webltga.espe.edu.ec/sg-telefonia/index.php?r=site/login&token=" +
-                            localStorage.getItem("laravel-jwt-auth")
-                    }
-                ]
+                sgrad:
+                    "http://webltga.espe.edu.ec/sg-rad/index.php?r=site/login&token=" +
+                    localStorage.getItem("laravel-jwt-auth"),
+                otrs:
+                    "http://webltga.espe.edu.ec/sg-otrs/index.php?r=site/login&token=" +
+                    localStorage.getItem("laravel-jwt-auth"),
+
+                fin:
+                    "http://webltga.espe.edu.ec/comprobantes/index.php?r=site/login&token=" +
+                    localStorage.getItem("laravel-jwt-auth"),
+
+                ecp:
+                    "http://webltga.espe.edu.ec/sg-ecp/index.php?r=site/login&token=" +
+                    localStorage.getItem("laravel-jwt-auth"),
+
+                telefonia:
+                    "http://webltga.espe.edu.ec/sg-telefonia/index.php?r=site/login&token=" +
+                    localStorage.getItem("laravel-jwt-auth"),
+                reporteshistoricos:
+                    "http://webltga.espe.edu.ec/sg-ri/index.php?r=site/login&token=" +
+                    localStorage.getItem("laravel-jwt-auth"),
+                educacioncontinua:
+                    "http://webltga.espe.edu.ec/sg-uec/index.php?r=site/login&token=" +
+                    localStorage.getItem("laravel-jwt-auth")
             }
         };
     },
@@ -290,6 +167,7 @@ export default {
         axios.get(`${window.location.origin}/api/v1/auth/user`).then(res => {
             this.role = res.data.data.role;
             console.log(res.data.data.role);
+            console.log(this.items.doc);
         });
     }
 };
